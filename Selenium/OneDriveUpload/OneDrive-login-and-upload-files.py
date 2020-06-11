ee2325955610919bf2cce392b48c37f366992a57
pchmielecki <pchmielecki87@gmail.com>
2020-06-11 18:32:52 +0200

    onedrive

--- Selenium/OneDriveUpload/OneDrive-login-and-upload-files.py
+++ Selenium/OneDriveUpload/OneDrive-login-and-upload-files.py
@@ -4,18 +4,16 @@ from selenium import webdriver
 browser = webdriver.Chrome()
 browser.get("https://www.goodreads.com/")
 
-username = credentials.GOODREADS_USERNAME
-password = credentials.GOODREADS_PASSWORD
+#username = credentials.GOODREADS_USERNAME
+#password = credentials.GOODREADS_PASSWORD
 
-username_input = browser.find_element_by_xpath(//*[@id="userSignInFormEmail"])
-password_input = browser.find_element_by_xpath(//*[@id="user_password"])
+#username_input = browser.find_element_by_xpath(//*[@id="userSignInFormEmail"])
+#password_input = browser.find_element_by_xpath(//*[@id="user_password"])
 
+#username_input.send_keys(username)
+#username_input.send_keys(password)
 
-
-username_input.send_keys(username)
-username_input.send_keys(password)
-
-login_button = browser.find_element_by_xpath(//*[@id="sign_in"]/div[3]/input[1])0
+login_button = browser.find_element_by_xpath(//*[@id="meControl"]/div/a)
 login_button.click()
 
 sleep(2)
