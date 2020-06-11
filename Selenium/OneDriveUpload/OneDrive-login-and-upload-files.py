d1b42c1d527370ab0e4f58cd043a702fd39e5824
pchmielecki87 <pchmielecki87@gmail.com>
2020-06-11 19:19:52 +0200

    works easy option

--- Selenium/OneDriveUpload/OneDrive-login-and-upload-files.py
+++ Selenium/OneDriveUpload/OneDrive-login-and-upload-files.py
@@ -1,19 +1,25 @@
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC, wait
 
 browser = webdriver.Chrome()
-browser.get("https://www.goodreads.com/")
+browser.get("https://my.clippings.io/#/login")
+browser.implicitly_wait(5)
 
 #username = credentials.GOODREADS_USERNAME
 #password = credentials.GOODREADS_PASSWORD
 
-#username_input = browser.find_element_by_xpath(//*[@id="userSignInFormEmail"])
-#password_input = browser.find_element_by_xpath(//*[@id="user_password"])
+username_input = browser.find_element_by_id("username")
+password_input = browser.find_element_by_id("password")
 
-#username_input.send_keys(username)
-#username_input.send_keys(password)
+username_input.send_keys("username")
+password_input.send_keys("password")
 
-login_button = browser.find_element_by_xpath(//*[@id="meControl"]/div/a)
+#import_button = browser.find_element_by_id("")
+
+login_button = browser.find_element_by_xpath("//button[@type='submit']")
 login_button.click()
 
 sleep(2)
