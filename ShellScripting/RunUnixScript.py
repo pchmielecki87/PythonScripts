d6ab2a4a085d781a545bf5e38b1d5a3e2c950641
pchmielecki87 <pchmielecki87@gmail.com>
2020-11-16 22:42:19 +0100

    shell commands

--- ShellScripting/RunUnixScript.py
+++ ShellScripting/RunUnixScript.py
@@ -5,3 +5,5 @@ os.system('ls -l')
 subprocess.run(["ls", "-l"]) # Run command
 subprocess.run(["ls", "-l"], stdout=subprocess.PIPE) # This will run the command and return any output
 subprocess.run(shlex.split("ls -l")) # You can also use the shlex library to split the command
+
+subprocess.run("systemctl restart nginx") #run nginx restart command
