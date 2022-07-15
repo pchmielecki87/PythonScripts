fe9090283f1f12deb4f35e42b0b2c2fdb04d7fd6
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2022-07-15 23:51:18 +0200

    selective view and save

---
+++ Pandas/pandasSelectiveExtractSave.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from openpyxl.workbook import Workbook
+
+df_csv = pd.read_csv('./data/Names.csv', header=None)
+
+# Add headers
+df_csv.columns = ['First', 'Last', 'Address', 'City', 'State', 'Code', 'Sth']
+
+# Save extracted data to file
+
+extracted_data = df_csv[['First', 'Last']]
+stored = extracted_data.to_excel('./data/modified/extracted_data.xlsx', header=None)
---
+++ Pandas/pandasSelectiveView.py
@@ -0,0 +1,21 @@
+import pandas as pd
+from openpyxl.workbook import Workbook
+
+df_csv = pd.read_csv('./data/Names.csv', header=None)
+
+# Add headers
+df_csv.columns = ['First', 'Last', 'Address', 'City', 'State', 'Code', 'Sth']
+
+# Selective viewing
+## one column
+print(df_csv['Last'])
+## two columns
+print(df_csv[['State', 'Code']])
+## rows 0-2 (first three)
+print(df_csv['Last'][0:3])
+
+#i-location function
+## print first entry (1 row, all columns)
+print(df_csv.iloc[1])
+## print 2nd row and 1st column (both starts from 0!!!)
+print(df_csv.iloc[2,1])
