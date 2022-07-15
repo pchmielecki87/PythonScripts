1a0bad1fd8de9c399142a5cff3157b0369f80f82
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2022-07-15 23:29:05 +0200

    pandas write to file

---
+++ Pandas/pandasWriteToExcel.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from openpyxl.workbook import Workbook
+
+df_csv = pd.read_csv('./data/Names.csv', header=None)
+
+# Add headers
+df_csv.columns = ['First', 'Last', 'Address', 'City', 'State', 'Code', 'Sth']
+
+# Save
+df_csv.to_excel('./data/modified/modified.xlsx')
+
+# print(df_csv)
