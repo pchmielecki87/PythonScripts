ae78523d2309940f831a6bd76fa24d7c2b83f0c6
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2022-07-16 10:45:26 +0200

    concat sheets

--- Excel/Pandas/pandasConcatSheets.py
+++ Excel/Pandas/pandasConcatSheets.py
@@ -10,3 +10,9 @@ df_all = pd.concat([df_1, df_2, df_3],sort=False)
 
 # Print
 print(df_all)
+
+# GroupBy each shift and how much they produced
+print(df_all.groupby(['Shift']).mean()['Units Sold'])
+
+# Export to Excel
+to_excel = df_all.to_excel('./data/modified/all_shifts.xlsx', index=None)
