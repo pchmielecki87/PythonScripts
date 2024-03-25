0eac08aa52eb968b213eb7d02a318424f392870e
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2024-03-25 11:35:03 +0100

    PCEP debug4

--- PCEP prep/data_structures.py
+++ PCEP prep/data_structures.py
@@ -21,7 +21,24 @@ print(list)
 list.insert(0,2)            # change element[0] from 1 to 2
 print(list)
 
-print("Atlassian stack: " + app1 + ", " + app2 + " and " + app3)
+list2 = [1,10,7,9,3,2,2,8,1,1,8,9,0]
+print(f"lista nieposortowana {list2}")
+list2.sort()
+print(f"lista posortowana {list2}")
+
+
+print(str(50))              # enforce int 50 will be string
+
+
+# print("Atlassian stack: " + app1 + ", " + app2 + " and " + app3)
+
+# user_input = input("Hey, napisz cos\n")
+# input()
+# user_input.split(";")
+# print(user_input)
+
+
+
 
 # Using escape characters to include special characters within a string
 escaped_string = "This is a \"quoted\" string."
