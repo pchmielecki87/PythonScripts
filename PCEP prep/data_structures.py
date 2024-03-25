9f8b7302521ccd1132b6550b6340aadf2cdab621
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2024-03-25 10:08:40 +0100

    PCEP debug3

--- PCEP prep/data_structures.py
+++ PCEP prep/data_structures.py
@@ -2,3 +2,31 @@ app1 = 'Jira'
 app2 = 'Confluence'
 app3 = 'Crowd'
 
+list = [1,'Samuel','pastor',1]
+set = {'1','Samuel','pastor',1,1,1}     # no-duplicates
+tuple = (1,1,1,3,4,5,7,9)               # immutable
+
+print(f"list: {list}")
+print(f"set: {set}")
+print(f"tuple: {tuple}")
+
+print(type(list[0]))        # type
+print(list[-1])             # print last
+print(list[:2])             # pint first two (index 0 and 1)
+print(list)
+
+list.append("JanDupa")      # add new element to the end
+print(list)
+
+list.insert(0,2)            # change element[0] from 1 to 2
+print(list)
+
+print("Atlassian stack: " + app1 + ", " + app2 + " and " + app3)
+
+# Using escape characters to include special characters within a string
+escaped_string = "This is a \"quoted\" string."
+print(escaped_string)
+
+# Another example with newline and tab escape characters
+multiline_string = "First line\nSecond line\n\tIndented line"
+print(multiline_string)
--- PCEP prep/operators.py
+++ PCEP prep/operators.py
@@ -1,12 +1,15 @@
-# x = 1
-# y = 2
+x = 1
+y = 2
 
-# if x == y:
-#     print("They're equal")
-#     print(x,y)
-# else:
-#     print("They're not equal")
-#     print(x,y)
+if x == y:
+    print("They're equal")
+    print(x,y)
+else:
+    print("They're not equal")
+    print(x,y)
+
+val1 = 1_000_000
+print(val1)
 
 # math
 val1 = 1/2
