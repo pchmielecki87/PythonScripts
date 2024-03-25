d7dc903e24fbc55d9f48207da4811e175f564754
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2024-03-25 08:47:33 +0100

    PCEP debug1

---
+++ PCEP prep/check_var_type.py
@@ -0,0 +1,7 @@
+var1 = "blablabla"
+var2 = 123
+var3 = True
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
---
+++ PCEP prep/variables_local_global.py
@@ -0,0 +1,7 @@
+hours_rate = 24
+
+def time_converter(minutes_rate):
+    # minutes_rate = 3
+    print(f"Hours number is {hours_rate} * {minutes_rate} minutes = {hours_rate * minutes_rate}")
+
+time_converter(5)
