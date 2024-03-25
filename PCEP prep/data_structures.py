6e594ec7dfaa0f913ff29b2e734654ad4f48fb27
Przemyslaw Chmielecki <pchmielecki87@gmail.com>
2024-03-25 09:12:00 +0100

    PCEP debug2

---
+++ PCEP prep/data_structures.py
@@ -0,0 +1,4 @@
+app1 = 'Jira'
+app2 = 'Confluence'
+app3 = 'Crowd'
+
---
+++ PCEP prep/operators.py
@@ -0,0 +1,17 @@
+# x = 1
+# y = 2
+
+# if x == y:
+#     print("They're equal")
+#     print(x,y)
+# else:
+#     print("They're not equal")
+#     print(x,y)
+
+# math
+val1 = 1/2
+val2 = 10//3    # return int from division
+val3 = 10%2     # return if odd/rest (1) or even/no-rest (0)
+val4 = 2**10    # 2^10
+
+print(val3)
